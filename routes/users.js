0bc93f0bb243518e3df0b27261d67bbf4f98f47b
LittleT0fu <chinchanathavornkraiwit@gmail.com>
2022-11-26 11:18:49 +0700

    WorkshopA

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('Hello with a resource');
+  res.status(200).json({
+    fullname : 'Kraiwit Chinchanathavorn'
+  })
 });
+
+router.get('/bio' , function(req, res, next){
+  res.status(200).json({
+    fullname : 'Kraiwit Chinchanathavorn',
+    nickname : 'Got',
+    hobby: 'sleep',
+    gitusername : 'LittleT0fu'
+
+  })
+})
+
+
 
 module.exports = router;
